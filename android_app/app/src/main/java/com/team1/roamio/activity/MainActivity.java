7041f91ae5830d9250c67049f19c3b2348d1c15d
주민재 <john_joo7@naver.com>
2025-10-31 23:08:02 +0900

    pakage setup

--- android_app/app/src/main/java/com/team1/roamio/activity/MainActivity.java
+++ android_app/app/src/main/java/com/team1/roamio/activity/MainActivity.java
@@ -1,14 +1,11 @@
-package com.team1.roamio;
+package com.team1.roamio.activity;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.genai.Client;
-import com.google.genai.types.GenerateContentResponse;
+import com.team1.roamio.R;
 
 
 public class MainActivity extends AppCompatActivity {
