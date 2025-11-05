205a56afdfc1c85275bfff281a4f2f221ff654b0
youngwooke <yamwook@naver.com>
2025-11-05 23:09:27 +0900

    Update PlanData.java
    TODO 정리

--- android_app/app/src/main/java/com/team1/roamio/data/PlanData.java
+++ android_app/app/src/main/java/com/team1/roamio/data/PlanData.java
@@ -4,21 +4,50 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class PlanData {
-    private JSONArray planDataJson;
-    // TODO: 2025-11-03 유저의 커스텀 정보 ex) 채류 기간, 여행 취향, 숙소 위치 등등
-
-    public PlanData() {
+    private JSONArray planDataJson; // TODO: 2025-11-03 유저의 커스텀 정보 ex) 체류 기간, 여행 취향, 숙소 위치 등등
+    private JSONObject userCustomData;
+    private PlanData() {
         planDataJson = new JSONArray();
+        userCustomData = new JSONObject();
     }
 
-    public static PlanData PlanDataBuilder() {
+    public static PlanData planDataBuilder() {
         return new PlanData();
     }
 
     // TODO: 2025-11-03 유저 커스텀 정보 setter(빌더 패턴)
-
-    public PlanData build() {
-        // TODO: 2025-11-03 프롬프트 실행
+    // 체류 기간
+    public PlanData setStayDuration(int days){
+        userCustomData.put("stay_duration", days);
         return this;
+    }
+    // 여행 취향
+    public PlanData setPreference(String preference){
+        userCustomData.put("travel_preference", preference);
+        return this;
+    }
+    // 숙소 위치(좌표)
+    /*
+    public PlanData setHotelLocation(JSONObject location){
+        userCustomData.put("hotel_location",location);
+        return this;
+    }
+    */
+    // 숙소 위치(String)
+    public PlanData setHotelLocation(String location){
+        userCustomData.put("hotel_location",location);
+        return this;
+    }
+    // 일정
+    public PlanData addPlan(JSONObject plan){
+        planDataJson.put(plan);
+        return this;
+    }
+    // 빌드
+    public JSONObject build(){
+        JSONObject result =  new JSONObject();
+        result.put("user_custom", userCustomData);
+        result.put("plans", planDataJson);
+        return result;
     }
 }
